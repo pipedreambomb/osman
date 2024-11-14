be7b42f30299c5cbebed59911efd0271ad714d6c
pipedreambomb <g@georgenixon.co.uk>
2024-11-14 16:07:46 +0000

    Made attemps littler

--- src/components/ContestantScore.tsx
+++ src/components/ContestantScore.tsx
@@ -5,22 +5,21 @@ type ContestantScoreProps = {
   isTurnTaker: boolean;
 }
 
-export function ContestantScore({contestant, isTurnTaker}: ContestantScoreProps) {
+export function ContestantScore({ contestant, isTurnTaker }: ContestantScoreProps) {
   return (
-    <div 
-      className={`flex justify-between items-center p-3 rounded-lg ${
-        isTurnTaker ? 'bg-yellow-100' : 'bg-white'
-      }`}
+    <div
+      className={`flex justify-between items-center p-3 rounded-lg ${isTurnTaker ? 'bg-yellow-100' : 'bg-white'
+        }`}
     >
-      <span className={`text-lg font-medium ${contestant.colour || 'text-gray-700'}`}>
+      <span className={`text-xl font-medium ${contestant.colour || 'text-gray-700'}`}>
         {contestant.name}
       </span>
-      <span className={`text-2xl font-bold ${contestant.colour || 'text-indigo-600'}`}>
+      <span className={`text-3xl font-bold ${contestant.colour || 'text-indigo-600'}`}>
         {contestant.score}
-      <span className={`text-l font-bold ${contestant.colour || 'text-indigo-600'}`}>
-        {`/${contestant.attempts}`}
+        <span className={`text-base font-bold ${contestant.colour || 'text-indigo-600'}`}>
+          {` /  ${contestant.attempts}`}
+        </span>
       </span>
-      </span>      
     </div>
   );
 }
